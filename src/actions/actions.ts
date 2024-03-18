349b5a408b97566a662cfba9b3badbdfd5c6733c
Khurram Ali <khurramcali@gmail.com>
2024-03-18 05:40:32 -0400

    security fix

---
+++ src/actions/actions.ts
@@ -0,0 +1,14 @@
+"use server";
+import prisma from "@/lib/db";
+
+export async function addPet(formData) {
+  await prisma.pet.create({
+    data: {
+      name: formData.get("name"),
+      ownerName: formData.get("ownerName"),
+      imageUrl: formData.get("imageUrl"),
+      age: Number(formData.get("age")),
+      notes: formData.get("notes"),
+    },
+  });
+}
